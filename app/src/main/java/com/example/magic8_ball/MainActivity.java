023c4a168cca039ab5e6a5cc95f16a1bc726e367
Christian <christian.v.t.shadd@gmail.com>
2019-08-29 12:21:50 -0700

    Update

--- app/src/main/java/com/example/magic8_ball/MainActivity.java
+++ app/src/main/java/com/example/magic8_ball/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.magic8_ball;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -20,28 +22,35 @@ public class MainActivity extends AppCompatActivity {
 
         ball = new Ball();
 
+        final Animation fadeInAnimation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fade_in);
+
+        final Button ballAskButton = (Button)findViewById(R.id.ball_ask_button);
         final Button ballClearButton = (Button)findViewById(R.id.ball_clear_button);
         final ImageView ballImageView = (ImageView)findViewById(R.id.ball_image_view);
         final TextView ballResponseTextView = (TextView)findViewById(R.id.ball_response_text_view);
-        final Button ballShakeButton = (Button)findViewById(R.id.ball_shake_button);
         final ImageView ballTriangleImageView = (ImageView)findViewById(R.id.ball_triangle_image_view);
+        ballResponseTextView.setAlpha(0.0f);
+        ballTriangleImageView.setAlpha(0.0f);
 
+        ballAskButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ballImageView.setImageResource(R.drawable.ball_2);
+                ballTriangleImageView.setAlpha(1.0f);
+                ballTriangleImageView.startAnimation(fadeInAnimation);
+                ballResponseTextView.setAlpha(1.0f);
+                ballResponseTextView.startAnimation(fadeInAnimation);
+                ballResponseTextView.setText(ball.shake());
+
+            }
+        });
         ballClearButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 ballImageView.setImageResource(R.drawable.ball);
-                ballResponseTextView.setText("");
-                ballTriangleImageView.setVisibility(View.INVISIBLE);
+                ballResponseTextView.setAlpha(0.0f);
+                ballTriangleImageView.setAlpha(0.0f);
             }
         });
-        ballShakeButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                ballImageView.setImageResource(R.drawable.ball_2);
-                ballResponseTextView.setText(ball.shake());
-                ballTriangleImageView.setVisibility(View.VISIBLE);
-            }
-        });
-        ballTriangleImageView.setVisibility(View.INVISIBLE);
     }
 }
